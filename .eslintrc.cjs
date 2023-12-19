dd8532b6ccdda746ad1d9cc2627a602fcbcb06ac
1251912798 <1251912798@qq.com>
2023-12-19 15:04:33 +0800

    style:add prettier、eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,14 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2020: true,
+  },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
@@ -12,7 +16,11 @@ module.exports = {
   rules: {
     'react-refresh/only-export-components': [
       'warn',
-      { allowConstantExport: true },
+      {
+        allowConstantExport: true,
+      },
     ],
+    'no-unused-vars': 'warn', // 禁止定义未使用的变量
+    'no-var': 'error', // 要求使用 let 或 const 而不是 var
   },
-}
+};
